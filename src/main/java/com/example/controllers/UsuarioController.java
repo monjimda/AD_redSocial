33fbf1027063fbb277809d82893e91ce2a2999cb
monjimda <d.monco@alumnos.upm.es>
2016-05-20 21:07:49 +0200

    cast array

--- src/main/java/com/example/controllers/UsuarioController.java
+++ src/main/java/com/example/controllers/UsuarioController.java
@@ -74,9 +74,9 @@ public class UsuarioController {
 		imagenByte = Files.readAllBytes(Paths.get(Config.getInstance().getProperty(Config.PATH_IMAGENES)+"/perfilPorDefecto.png"));
 		archivo = new File(Config.getInstance().getProperty(Config.PATH_IMAGENES)+"/"+resource.getNick()+"/perfil.png");
 		FileUtils.writeByteArrayToFile(archivo,imagenByte);
-		String[] fotos = null;
-		fotos[0] = "perfil";
-		resource.setFotos(fotos);
+		List fotos = new ArrayList<String>();
+		fotos.add("perfil");
+		resource.setFotos((String[]) fotos.toArray());
 		resource.setPassword(this.makePasswordHash(resource.getPassword(), this.generateSalting()));
 		dao.createUsuario(resource);
 		return resource;
